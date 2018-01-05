435d216c5d8c7a1b20292fb1759c181fbe25db2f
Tamal Saha <tamal@appscode.com>
2018-01-05 00:05:51 -0800

    Delete patch-apiserver.sh

--- minikube/1.9/auditing/patch-apiserver.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -xeuo pipefail
-
-curl -o kubernetes-client-linux-amd64.tar.gz https://dl.k8s.io/v1.9.0/kubernetes-client-linux-amd64.tar.gz \
-  && tar -xzvf kubernetes-client-linux-amd64.tar.gz
