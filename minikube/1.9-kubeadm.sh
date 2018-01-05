f8ad8c8d76488efc14c9d133d28b8a230b84d397
Tamal Saha <tamal@appscode.com>
2018-01-04 20:43:55 -0800

    Create 1.9-kubeadm.sh

---
+++ minikube/1.9-kubeadm.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+minikube delete
+# https://github.com/kubernetes/kubeadm/issues/629
+minikube start \
+  --kubernetes-version=v1.9.0 \
+  --bootstrapper=kubeadm \
+  --extra-config=apiserver.admission-control="NamespaceLifecycle,LimitRanger,ServiceAccount,PersistentVolumeLabel,DefaultStorageClass,ValidatingAdmissionWebhook,ResourceQuota,DefaultTolerationSeconds,MutatingAdmissionWebhook"
+ 
