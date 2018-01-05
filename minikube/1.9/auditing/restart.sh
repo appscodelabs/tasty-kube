942bd62786aead86adbd5339891e069a02d6bcc7
Tamal Saha <tamal@appscode.com>
2018-01-04 21:30:44 -0800

    Update and rename minikube/1.9/a3.sh to minikube/1.9/auditing/restart.sh

--- minikube/1.9/auditing/restart.sh
+++ minikube/1.9/auditing/restart.sh
@@ -2,17 +2,9 @@
 
 set -xeou pipefail
 
-cat <<EOF > ~/.minikube/files/audit-policy.yaml
-# Log all requests at the Metadata level.
-apiVersion: audit.k8s.io/v1beta1
-kind: Policy
-rules:
-- level: Metadata
-EOF
-
 minikube delete || true
 minikube start \
 --kubernetes-version=v1.9.0 \
 --bootstrapper=kubeadm --extra-config=apiserver.admission-control="NamespaceLifecycle,LimitRanger,ServiceAccount,PersistentVolumeLabel,DefaultStorageClass,ValidatingAdmissionWebhook,ResourceQuota,DefaultTolerationSeconds,MutatingAdmissionWebhook" \
 --feature-gates=AdvancedAuditing=true \
---extra-config=apiserver.audit-log-path=/tmp/audit.log
+--extra-config=apiserver.audit-log-path=/tmp/data/audit.log
