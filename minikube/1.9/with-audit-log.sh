8993d0f5b0bae98462cb616109e51ac4bae654f4
Tamal Saha <tamal@appscode.com>
2018-01-04 20:47:34 -0800

    Create with-audit-log.sh

---
+++ minikube/1.9/with-audit-log.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cat <<EOF > ~/.minikube/files/audit-policy.yaml
+# Log all requests at the Metadata level.
+apiVersion: audit.k8s.io/v1beta1
+kind: Policy
+rules:
+- level: Metadata
+EOF
+
+minikube delete
+minikube start \
+--kubernetes-version=v1.9.0 \
+--bootstrapper=kubeadm --extra-config=apiserver.admission-control="NamespaceLifecycle,LimitRanger,ServiceAccount,PersistentVolumeLabel,DefaultStorageClass,ValidatingAdmissionWebhook,ResourceQuota,DefaultTolerationSeconds,MutatingAdmissionWebhook" \
+--extra-config=apiserver.audit-policy-file=/audit-policy.yaml \
+--extra-config=apiserver.audit-log-path=/tmp/audit.log
+--feature-gates=AdvancedAudit=true 
