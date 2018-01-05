13f0d614bce8c05e2f5b08375ea00317e60b4b30
Tamal Saha <tamal@appscode.com>
2018-01-04 20:53:08 -0800

    Update with-audit-log.sh

--- minikube/1.9/with-audit-log.sh
+++ minikube/1.9/with-audit-log.sh
@@ -14,6 +14,6 @@ minikube delete
 minikube start \
 --kubernetes-version=v1.9.0 \
 --bootstrapper=kubeadm --extra-config=apiserver.admission-control="NamespaceLifecycle,LimitRanger,ServiceAccount,PersistentVolumeLabel,DefaultStorageClass,ValidatingAdmissionWebhook,ResourceQuota,DefaultTolerationSeconds,MutatingAdmissionWebhook" \
+--feature-gates=AdvancedAuditing=true \
 --extra-config=apiserver.audit-policy-file=/audit-policy.yaml \
 --extra-config=apiserver.audit-log-path=/tmp/audit.log
---feature-gates=AdvancedAuditing=true 
