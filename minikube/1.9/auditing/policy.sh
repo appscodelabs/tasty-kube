127d9370ff68f8c06e4466f11aed5c5e66dbabb2
Tamal Saha <tamal@appscode.com>
2018-01-04 21:46:51 -0800

    Delete policy.sh

--- minikube/1.9/auditing/policy.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-cat <<EOF > ~/.minikube/files/audit-policy.yaml
-# Log all requests at the Metadata level.
-apiVersion: audit.k8s.io/v1beta1
-kind: Policy
-rules:
-- level: Metadata
-EOF
