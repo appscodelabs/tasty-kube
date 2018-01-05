85f994445f0a310779649753f70f144458022704
Tamal Saha <tamal@appscode.com>
2018-01-04 22:35:12 -0800

    Update patch-apiserver.sh

--- minikube/1.9/auditing/patch-apiserver.sh
+++ minikube/1.9/auditing/patch-apiserver.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -xeuo pipefail
 
-wget https://dl.k8s.io/v1.9.0/kubernetes-client-linux-amd64.tar.gz \
+curl -o kubernetes-client-linux-amd64.tar.gz https://dl.k8s.io/v1.9.0/kubernetes-client-linux-amd64.tar.gz \
   && tar -xzvf kubernetes-client-linux-amd64.tar.gz
