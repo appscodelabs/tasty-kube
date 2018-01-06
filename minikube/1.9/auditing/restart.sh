334e1905344bd687effcf53ac50ea41432d356ce
aerokite <aerofoil.kite@gmail.com>
2018-01-06 15:53:51 +0600

    change minikube mount directory

--- minikube/1.9/auditing/restart.sh
+++ minikube/1.9/auditing/restart.sh
@@ -2,14 +2,12 @@
 
 set -xeou pipefail
 
-mkdir -p ~/.minikube/files
-curl -fsSL https://raw.githubusercontent.com/tamalsaha/tasty-kube/master/minikube/1.9/auditing/kube-apiserver.yaml > ~/.minikube/files/kube-apiserver.yaml
-curl -fsSL https://raw.githubusercontent.com/tamalsaha/tasty-kube/master/minikube/1.9/auditing/audit-policy.yaml > ~/.minikube/files/audit-policy.yaml
+mkdir -p ~/.minikube/files/audit
+curl -fsSL https://raw.githubusercontent.com/tamalsaha/tasty-kube/master/minikube/1.9/auditing/audit-policy.yaml > ~/.minikube/files/audit/audit-policy.yaml
 
 minikube delete || true
 minikube start \
 --kubernetes-version=v1.9.0 \
 --bootstrapper=kubeadm --extra-config=apiserver.admission-control="NamespaceLifecycle,LimitRanger,ServiceAccount,DefaultStorageClass,ValidatingAdmissionWebhook,ResourceQuota,DefaultTolerationSeconds,MutatingAdmissionWebhook" \
---mount --mount-string="$HOME/.minikube/files:/tmp/files" \
---feature-gates=AdvancedAuditing=true \
---extra-config=apiserver.audit-log-path=/tmp/files/audit.log
+--mount --mount-string="$HOME/.minikube/files:/.minikube/files" \
+--feature-gates=AdvancedAuditing=true
