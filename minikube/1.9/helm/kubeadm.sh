d16c887b0e065be3a767d94c0640d7b8b2773bdb
Tamal Saha <tamal@appscode.com>
2018-03-04 17:12:23 -0800

    Create kubeadm.sh

---
+++ minikube/1.9/helm/kubeadm.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+kubectl -n kube-system create sa tiller
+kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
+helm init --service-account tiller
