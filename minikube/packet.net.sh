0c4864f64b986ede32013c54844c6b15572605fe
Tamal Saha <tamal@appscode.com>
2018-03-29 09:37:14 -0700

    Update packet.net.sh

--- minikube/packet.net.sh
+++ minikube/packet.net.sh
@@ -10,4 +10,10 @@ apt install dkms linux-headers-$(uname -r) virtualbox
 # 1.8.0
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.24.1/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
 
+wget https://dl.k8s.io/v1.8.10/kubernetes-client-linux-amd64.tar.gz \
+  && tar -zxvf kubernetes-client-linux-amd64.tar.gz \
+  && mv kubernetes/client/bin/* /usr/local/bin
+
 minikube delete; minikube start
+kubectl get nodes
+
