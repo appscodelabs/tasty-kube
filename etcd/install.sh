82c3ef52043cdfb211e8ca0d1090ef1ee017b6b0
Tamal Saha <tamal@appscode.com>
2018-04-01 23:04:29 -0700

    Create install.sh

---
+++ etcd/install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+ETCD_VER=v3.2.18
+
+# choose either URL
+GOOGLE_URL=https://storage.googleapis.com/etcd
+GITHUB_URL=https://github.com/coreos/etcd/releases/download
+DOWNLOAD_URL=${GOOGLE_URL}
+
+rm -f /opt/etcd-${ETCD_VER}-linux-amd64.tar.gz
+rm -rf /opt/etcd-download-test && mkdir -p /opt/etcd-download-test
+
+curl -L ${DOWNLOAD_URL}/${ETCD_VER}/etcd-${ETCD_VER}-linux-amd64.tar.gz -o /opt/etcd-${ETCD_VER}-linux-amd64.tar.gz
+tar xzvf /opt/etcd-${ETCD_VER}-linux-amd64.tar.gz -C /opt/etcd-${ETCD_VER}-linux-amd64 --strip-components=1
+rm -f /opt/etcd-${ETCD_VER}-linux-amd64.tar.gz
+
+/opt/etcd-${ETCD_VER}-linux-amd64/etcd --version
+<<COMMENT
+etcd Version: 3.2.18
+Git SHA: eddf599c6
+Go Version: go1.8.7
+Go OS/Arch: linux/amd64
+COMMENT
+
+ETCDCTL_API=3 /opt/etcd-${ETCD_VER}-linux-amd64/etcdctl version
+<<COMMENT
+etcdctl version: 3.2.18
+API version: 3.2
+COMMENT
