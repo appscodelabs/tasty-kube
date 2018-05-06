a3fe5bcf94fe199bf2cb33c7657502b22d9c91ee
tamal <tamal@appscode.com>
2018-05-06 13:00:13 -0700

    added-all

--- minikube/1.10/psp/role-bindings.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -x
-
-kubectl create rolebinding psp:kube-dns --clusterrole=psp:privilged --serviceaccount=kube-system:kube-dns --namespace=kube-system
-kubectl create rolebinding psp:kube-proxy --clusterrole=psp:privilged --serviceaccount=kube-system:kube-proxy --namespace=kube-system
-kubectl create rolebinding psp:default --clusterrole=psp:privilged --serviceaccount=kube-system:default --namespace=kube-system
-kubectl create rolebinding psp:storage-provisioner --clusterrole=psp:privilged --serviceaccount=kube-system:storage-provisioner --namespace=kube-system
