4096bb345489c32cc2be0b821eeef3c29514cca4
Tamal Saha <tamal@appscode.com>
2020-06-01 03:29:30 -0700

    added-all

--- minikube/psp/restart.sh
+++ minikube/psp/restart.sh
@@ -3,5 +3,6 @@ set -xeou pipefail
 
 minikube delete || true
 minikube start \
+  --driver=virtualbox \
   --kubernetes-version=v1.18.3 \
   --extra-config=apiserver.enable-admission-plugins="NamespaceLifecycle,LimitRanger,ServiceAccount,TaintNodesByCondition,Priority,DefaultTolerationSeconds,DefaultStorageClass,StorageObjectInUseProtection,PersistentVolumeClaimResize,RuntimeClass,CertificateApproval,CertificateSigning,CertificateSubjectRestriction,DefaultIngressClass,MutatingAdmissionWebhook,ValidatingAdmissionWebhook,ResourceQuota,PodSecurityPolicy"
