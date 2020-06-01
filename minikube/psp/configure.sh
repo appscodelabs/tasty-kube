d643399f6c6748ce658c7904ec256799ae3bd34e
Tamal Saha <tamal@appscode.com>
2020-06-01 11:56:40 -0700

    added-all

--- minikube/psp/configure.sh
+++ minikube/psp/configure.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-kubectl create -f https://raw.githubusercontent.com/kubernetes/website/master/content/en/examples/policy/privileged-psp.yaml
-kubectl create -f https://raw.githubusercontent.com/kubernetes/website/master/content/en/examples/policy/baseline-psp.yaml
-kubectl create -f https://raw.githubusercontent.com/kubernetes/website/master/content/en/examples/policy/restricted-psp.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/website/master/content/en/examples/policy/privileged-psp.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/website/master/content/en/examples/policy/baseline-psp.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/website/master/content/en/examples/policy/restricted-psp.yaml
 
-kubectl create -f ./cluster-roles.yaml
-kubectl create -f ./role-bindings.yaml
+kubectl apply -f ./cluster-roles.yaml
+kubectl apply -f ./role-bindings.yaml
